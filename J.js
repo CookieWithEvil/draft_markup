5045cdd26020a522dadd5cfb668c336d303bfe9a
Mita <mitafrumoasa@gmail.com>
2018-08-19 17:10:00 +0300

    some draft

---
+++ J.js
@@ -0,0 +1,8 @@
+function hideMenu(){
+	var x = document.getElementById('menu');
+    if (x.style.visibility === 'hidden') {
+        x.style.visibility = 'visible';
+    } else {
+        x.style.visibility = 'hidden';
+    }
+}
